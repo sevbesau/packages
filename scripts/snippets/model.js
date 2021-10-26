899bd22924f6a80f760cd84db17fe4d620f087e3
sevbesau <seppe@sevbesau.xyz>
2021-10-26 12:54:46 +0200

    store generation

--- scripts/snippets/model.js
+++ scripts/snippets/model.js
@@ -6,6 +6,66 @@ const Schema = new mongoose.Schema({
   timestamps: true,
 });
 
+
+/*
+// validate
+Schema.pre('validate', function () {
+  console.log('pre validate');
+});
+Schema.post('validate', function () {
+  console.log('post validate');
+});
+
+// find
+Schema.pre('find', function () {
+  console.log('pre find');
+});
+Schema.post('find', function () {
+  console.log('post find');
+});
+
+// findOne
+Schema.pre('findOne', function () {
+  console.log('pre findOne');
+});
+Schema.post('findOne', function () {
+  console.log('post findOne');
+});
+
+// save
+Schema.pre('save', function () {
+  console.log('pre save');
+});
+Schema.post('save', function () {
+  console.log('post save');
+});
+
+// update
+Schema.pre('update', function () {
+  console.log('pre update');
+});
+Schema.post('update', function () {
+  console.log('post update');
+});
+Schema.pre('updateOne', function () {
+  console.log('pre updateOne');
+});
+Schema.post('updateOne', function () {
+  console.log('post updateOne');
+});
+
+// delete
+Schema.pre('delete', function () {
+  console.log('pre delete');
+});
+Schema.post('delete', function () {
+  console.log('post delete');
+});
+*/
+
+
+
+
 const Model = mongoose.model('{{name}}', Schema);
 module.exports = Model;
 `;
--- scripts/snippets/store.js
+++ scripts/snippets/store.js
@@ -1,1 +1,121 @@
-module.exports = ``;
+module.exports = {
+  crud: `import envalidate from '@siliconminds/envalidate';
+import Api from '@siliconminds/api';
+import { required, } from '@siliconminds/validation/validators';
+
+envalidate('VUE_APP_API_URL');
+
+const api = new Api(process.env.VUE_APP_API_URL);
+
+const endpoint = '{{name}}';
+
+export default {
+  namespaced: true,
+  state: {
+    pagination: {
+      limit: 25,
+      prev: false,
+      page: 1,
+      next: false,
+      last: false,
+    },
+    fields: [
+      {
+        label: '',
+        key: '',
+        type: '',
+        editable: true,
+        validators: { required },
+      },
+    ],
+    all: [],
+    single: {},
+  },
+  getters: {
+    fields: (state) => state.fields,
+
+    all: (state) => state.all,
+    single: (state) => state.single,
+
+    pagination: (state) => state.pagination,
+  },
+  actions: {
+    async set_page({ dispatch, commit, getters }, page) {
+      const { pagination } = getters;
+      if ((page < 0 && pagination.page > 1) || (page > 0 && pagination.page < pagination.last)) {
+        commit('setPage', page);
+      }
+      dispatch('get_all');
+    },
+
+    async get_all({ commit, getters }, payload = {}) {
+      commit('clearError', null, { root: true });
+      const { pagination } = getters;
+      const res = await api.get(endpoint, { query: {
+        ...payload,
+        _page: pagination.page,
+        _limit: pagination.limit,
+      } });
+      if (!res.success) return commit('pushError', res.error, { root: true });
+      commit('setPagination', res.pagination);
+      return commit('setAll', res.data);
+    },
+    async get_single({ commit }, id) {
+      commit('clearError', null, { root: true });
+      const res = await api.get(\`\${endpoint}/\${id}\`);
+      if (!res.success) return commit('pushError', res.error, { root: true });
+      return commit('setSingle', res.data);
+    },
+    async post({ commit }, payload) {
+      commit('clearError', null, { root: true });
+      const res = await api.post(endpoint, payload);
+      if (!res.success) return commit('pushError', res.error, { root: true });
+      return commit('push', res.data);
+    },
+    async put({ commit }, payload) {
+      commit('clearError', null, { root: true });
+      const res = await api.put(\`\${endpoint}/\${payload._id}\`, payload);
+      if (!res.success) return commit('pushError', res.error, { root: true });
+      return commit('update', payload);
+    },
+    async delete({ commit, getters, dispatch }, payload) {
+      commit('clearError', null, { root: true });
+      const res = await api.delete(\`\${endpoint}/\${payload._id}\`);
+      if (!res.success) return commit('pushError', res.error, { root: true });
+      commit('delete', payload);
+      if (getters.pagination.page > 1 && getters.all.length < 1) return dispatch('set_page', -1);
+      return true;
+    },
+  },
+  mutations: {
+    setAll: (state, items) => { state.all = items; },
+    setSingle: (state, item) => { state.single = item; },
+    push: (state, item) => { state.all.push(item); },
+    update: (state, item) => { state.all[state.all.findIndex((el) => el._id.toString() === item._id.toString())] = item; },
+    delete: (state, item) => { state.all = state.all.filter(el => el._id.toString() !== item._id.toString()); },
+
+    setPagination: (state, pagination) => { state.pagination = pagination; },
+    setPage: (state, page) => { state.pagination.page += page; },
+  },
+};
+`,
+  blank: `export default {
+  // state for the store
+  state: {
+
+  },
+  // getters for components
+  getters: {
+
+  },
+  // async actions called by components
+  actions: {
+
+  },
+  // commit changes to the store
+  mutations: {
+
+  },
+};
+`
+}
